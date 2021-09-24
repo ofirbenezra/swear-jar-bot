6d18f0bbdb679f19a05cb53e6db8b1e804a835e5
Ofir Ben Ezra <ofir.benezra@gmail.com>
2021-09-24 14:29:29 +0300

    Fix version on github

--- index.js
+++ index.js
@@ -9,49 +9,36 @@ const profanityChecker = require('./profanity-checker');
 dotenv.config();
 const token = process.env.DISCORD_BOT_TOKEN;
 console.log('Stating the Bot');
-const client = new Client({ 
+const client = new Client({
    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
 });
 
 
 client.on('ready', () => {
-   // List servers the bot is connected to
-   // client.guilds.cache.forEach((guild) => {
-   //    console.log(" - " + guild.name + '' + guild.id)
-
-
-   //    // List all channels
-   //    guild.channels.cache.forEach((channel) => {
-   //       console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
-   //    })
-   // })
-   // const Guilds = client.guilds.cache.map(guild => guild.id);
-   //  console.log(Guilds);
+   console.log('Server is ready');
 })
 
 client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+   if (!interaction.isCommand()) return;
 
-	const { commandName } = interaction;
+   const { commandName } = interaction;
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply('Server info.');
-	} else if (commandName === 'user') {
-		await interaction.reply('User info.');
-	}
+   if (commandName === 'ping') {
+      await interaction.reply('Pong!');
+   }
 });
 
 client.on('messageCreate', (message) => {
-   profanityChecker.checkProfanityInText(message.content).then(res => {
-      if(res.indexOf('*') > -1){
-         console.log('swear found. placing swear jar link');
-         message.reply(`Oops, #$@! Found. Would you like to donate to machitv's SwearJar? Click here: https://streamelements.com/machitv/tip`);
-         // const exampleEmbed = new MessageEmbed().setTitle('Oops, #$?! Found. Would you like to donate to machitv\'s SwearJar?');
-         // message.channel.send({ embeds: [exampleEmbed] });
-      }
-   });
+   const botDisabled = process.env.DISABLE_BOT === 'false' ? false : true;
+   console.log(`Bot disbabled: ${botDisabled}`);
+   if (botDisabled === false) {
+      profanityChecker.checkProfanityInText(message.content).then(res => {
+         if (res && res.indexOf('*') > -1) {
+            console.log('swear found. placing swear jar link');
+            message.reply(`Oops, #$@! Found. Would you like to donate to machitv's SwearJar? Click here: https://streamelements.com/machitv/tip`);
+         }
+      });
+   }
 })
 
 client.login(token);
--- profanity-checker.js
+++ profanity-checker.js
@@ -1,13 +1,20 @@
 
 var axios = require("axios").default;
+const addtionalWords = [
+    'wtf', 'lmao', 'lmfao', 'af', 'roflmao', 
+    'bamf', 'fml', 'gtfo', 'omfg', 'pita'
+    // 'stfu', 'gtfo', 
+    // 'atfo', 'cya', 'rtfm', 'dtf', 'snb', 'bfr', 'milf',
+    // 'fyfi', 'mofo', 'mofos'
+];
 
 const checkProfanityInText = (textForCheck) => {
     var options = {
-      method: 'GET',
-      url: 'https://www.purgomalum.com/service/json',
-      params: {text: textForCheck}
+        method: 'GET',
+        url: 'https://www.purgomalum.com/service/json',
+        params: { text: textForCheck, add: addtionalWords.toString() }
     };
-    
+
     return axios.request(options).then(function (response) {
         return response.data.result;
     }).catch(function (error) {
