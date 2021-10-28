f1becfae0a8a599dd68f321275ff3dc97b1988d3
Ofir Ben Ezra <ofir.benezra@gmail.com>
2021-10-28 19:36:23 +0300

    restore new version

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const dotenv = require('dotenv');
 const profanityChecker = require('./profanity-checker');
+const { donation_links } = require('./server-data.json');
+const { Console } = require("console");
+const { link } = require("fs");
 
+const messagesArray = [
+   `Oops, #$@! Found. Would you like to donate to {serverName}'s SwearJar? <{link}>`,
+   `Sensitive ears alert! How about a donation to {serverName}'s SwearJar? <{link}>`,
+   `Toxins detected. Drop a $ to {serverName}'s SwearJar? <{link}>`,
+   `Whoa Nelly, that's quite a mouth on you. Donate to {serverName}'s SwearJar? <{link}>`,
+   
+]
 dotenv.config();
 const token = process.env.DISCORD_BOT_TOKEN;
+const botDisabled = process.env.DISABLE_BOT === 'false' ? false : true;
 console.log('Stating the Bot');
 const client = new Client({
    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
@@ -16,6 +27,7 @@ const client = new Client({
 
 client.on('ready', () => {
    console.log('Server is ready');
+   console.log(`Bot disbabled: ${botDisabled}`);
 })
 
 client.on('interactionCreate', async interaction => {
@@ -29,16 +41,47 @@ client.on('interactionCreate', async interaction => {
 });
 
 client.on('messageCreate', (message) => {
-   const botDisabled = process.env.DISABLE_BOT === 'false' ? false : true;
-   console.log(`Bot disbabled: ${botDisabled}`);
-   if (botDisabled === false) {
+   const donationObj = getServerNameAndDonationLink(message, client);
+   console.log(`*********Checking profanity on message '${message.content}', Server name: '${donationObj ?donationObj.serverName : 'not found'}'*******`);
+   if (botDisabled === false) {      
       profanityChecker.checkProfanityInText(message.content).then(res => {
-         if (res && res.indexOf('*') > -1) {
-            console.log('swear found. placing swear jar link');
-            message.reply(`Oops, #$@! Found. Would you like to donate to machitv's SwearJar? Click here: https://streamelements.com/machitv/tip`);
-         }
+         if(res) {
+            const words = res.split(" ");
+            const isProfnaityWord = words.find(word => word.indexOf("*") === 0 && 
+                                    word.lastIndexOf("*") === word.length - 1);
+            if (isProfnaityWord && isProfnaityWord.length > 0 && donationObj) {
+               console.log('swear found. placing swear jar link');
+               let randomMessage = messagesArray[Math.floor(Math.random() * messagesArray.length)];
+               randomMessage = replaceTokenInMessage(donationObj, randomMessage);
+               message.reply(randomMessage);
+            }
+         }         
       });
    }
 })
+
+function replaceTokenInMessage(donationObj, message) {
+   const regex = /{[\w]*\}/g;
+   let textTokens = message.match(regex);
+
+   if(textTokens.length > 0 && textTokens.length === 2) {
+      message = message.replace(textTokens[0], donationObj.serverName);
+      message = message.replace(textTokens[1], donationObj.link);
+   }
+   return message;
+}
+
+function getServerNameAndDonationLink(message, client) {
+   let donationLinkObj;
+   const serverName = client.guilds.cache.get(message.guild.id).name;
+   console.log(`**** server name is: ${serverName}`);
+   if(serverName) {
+      donationLinkObj = donation_links.find(x => x.serverName === serverName);
+   }
+   if(donationLinkObj) {
+      return donationLinkObj;
+   }
+  return null;
+}
 
 client.login(token);
