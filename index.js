bc16bae2c78eba5cdab7b67248703f04dc49ba6c
Ofir Ben Ezra <ofir.benezra@gmail.com>
2021-12-08 16:56:47 +0200

    Delete entry from db when a user is banned

--- index.js
+++ index.js
@@ -29,9 +29,7 @@ const client = new Client({
       Intents.FLAGS.GUILD_MESSAGES, 
       Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
       Intents.FLAGS.GUILD_MEMBERS,
-      // Intents.FLAGS.GUILD_MEMBER_REMOVE,
-      Intents.FLAGS.GUILD_BANS,
-      // Intents.FLAGS.GUILD_BAN_REMOVE
+      // Intents.FLAGS.GUILD_BANS
    ],
 });
 
